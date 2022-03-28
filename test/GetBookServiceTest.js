3c88c3203a96717c6cae3541608090c4d834c375
HappyFeed <alejomam2011@gmail.com>
2022-03-28 08:01:09 -0500

    Get test done

---
+++ test/GetBookServiceTest.js
@@ -0,0 +1,41 @@
+const  axios = require('axios');
+const chai = require('chai');
+const { assert } = require('chai');
+
+const { expect } = chai;
+
+describe ('Test GET request', async() => {
+
+    const apiCall = axios.create({
+        baseURL: "http://localhost:8080",
+    });
+
+    it('GET services complete the service correct', async() => {
+        
+        const response = await apiCall.get('/books');
+
+        expect(response.status).to.equal(200);
+    });
+
+    it('GET services should not return empty information', async() => {
+        
+        const response = await apiCall.get('/books');
+
+        expect(response.data).to.not.be.empty;
+    });
+
+    it('GET services should return all the books', async() => {
+        
+        const responseGet1 = await apiCall.get('/books');
+
+        const response = await apiCall.post('/books',{name:'NameGet', author:'AuthorGet'});
+
+        const responseGet2 = await apiCall.get('/books');
+    
+        assert.isTrue(responseGet2.data.length > responseGet1.data.length);
+
+        apiCall.delete('/books/' + response.data.id);
+    });
+
+
+});
